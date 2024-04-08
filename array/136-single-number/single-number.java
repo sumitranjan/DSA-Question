38dbaa79b2d21656f3b867e1a6ca21d4da2a07c9
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-04-08 23:20:38 +0530

    Time: 1 ms (99.85%) | Memory: 46.2 MB (23.57%) - LeetSync

--- array/136-single-number/single-number.java
+++ array/136-single-number/single-number.java
@@ -1,31 +1,9 @@
-import java.util.HashMap;
-
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-
-        // Declare the HasMap
-        // And hash the given array:
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            int value = map.getOrDefault(nums[i], 0);
-            map.put(nums[i], value+1);
+        int xor = 0;
+        for(int i=0; i<nums.length; i++){
+            xor = xor ^ nums[i];
         }
-
-        
-        
-        //Find the single element and return the answer:
-        for (int it : map.keySet()) {
-            if (map.get(it) == 1)
-                return it;
-        }
-
-        //This line will never execute
-        //if the array contains a single element.
-        return 0;
-
+        return xor;
     }
 }
-
-//This will not work if contains negative numbers or a very large number.
-//Use map data structure to hash the array elements.
