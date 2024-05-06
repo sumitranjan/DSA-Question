31b08903224c486c09b97b1ff31b94a37a90e406
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-05-06 18:10:56 +0530

    Time: 12 ms (34.60%) | Memory: 8.1 MB (78.96%) - LeetSync

--- array/9-palindrome-number/palindrome-number.cpp
+++ array/9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
+        // Handle negative numbers and numbers ending with 0 (except 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
         int revNo = 0;
         int num = x;
         while(x>0){
             int lastDigit = x % 10;
+            // Check for integer overflow or underflow
             if (revNo > INT_MAX / 10 || (revNo == INT_MAX / 10 && lastDigit > INT_MAX % 10)) {
-            return false;
-        }
+                return false;
+            }
             revNo = revNo * 10 +  lastDigit;
             x = x/10;
         }
