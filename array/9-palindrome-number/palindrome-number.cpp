aa06f96a24d6abf4e74124578e388788543ba16b
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-05-06 18:49:14 +0530

    Time: 15 ms (20.47%) | Memory: 8.1 MB (96.34%) - LeetSync

--- array/9-palindrome-number/palindrome-number.cpp
+++ array/9-palindrome-number/palindrome-number.cpp
@@ -5,18 +5,13 @@ public:
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        int revNo = 0;
-        int num = x;
-        while(x>0){
+        int b = 0;
+        while(x>b){
             int lastDigit = x % 10;
-            // Check for integer overflow or underflow
-            if (revNo > INT_MAX / 10 || (revNo == INT_MAX / 10 && lastDigit > INT_MAX % 10)) {
-                return false;
-            }
-            revNo = revNo * 10 +  lastDigit;
+            b = b * 10 +  lastDigit;
             x = x/10;
         }
-        return num == revNo;
+        return (x == b) || (x == b/10);
 
     }
 };
