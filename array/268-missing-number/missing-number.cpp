664dad42579cefd110b3e6044120cfc2af4e5587
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-05-19 13:55:55 +0530

    Time: 13 ms (59.38%) | Memory: 20.4 MB (57.03%) - LeetSync

--- array/268-missing-number/missing-number.cpp
+++ array/268-missing-number/missing-number.cpp
@@ -4,13 +4,10 @@ public:
         int n = nums.size();
         int xor1 = 0;
         int xor2 = 0;
-        for(int i=1; i<=n; i++){
-            xor1 = xor1 ^ i;
-        }
         for(int i=0; i<n; i++){
+            xor1 = xor1 ^ (i+1);
             xor2 = xor2 ^ nums[i];
         }
-
         return xor1 ^ xor2;
 
     }
