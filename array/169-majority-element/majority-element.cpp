66ea2d38e0e2d568539ccd81d6b8cfcd0b5ab084
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-06-05 23:54:51 +0530

    Time: 16 ms (68.53%) | Memory: 26.6 MB (70.64%) - LeetSync

--- array/169-majority-element/majority-element.cpp
+++ array/169-majority-element/majority-element.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        map<int, int> mpp;
-
-        //storing the elements with its occurnce:
-        for(int i=0; i<n; i++){
-            mpp[nums[i]]++;
-        }
-
-        //searching for the majority element:
-        for(auto it : mpp){
-            if(it.second > (n/2)){
-                return it.first;
+        int count = 0;
+        int el = nums[0];
+        for(int i=0; i<nums.size(); i++){
+            if(count == 0){
+                el = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == el){
+                count++;
+            }
+            else {
+                count--;
             }
         }
-        return -1;
+        
+        return el;
     }
 };
