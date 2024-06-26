f77c212a3416e1a22eb2194411df8693cdca6b44
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-06-27 00:54:04 +0530

    Time: 24 ms (77.23%) | Memory: 30 MB (23.44%) - LeetSync

--- array/792-binary-search/binary-search.cpp
+++ array/792-binary-search/binary-search.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(target < nums[mid])
-                high = mid - 1;
-            else
-                low = mid + 1; 
-        }
-        return -1;
+    int n = nums.size();
+    int low = 0, high = n-1;
+    while(low <= high){
+        int mid = low + (high-low)/2;
+        
+        // Check if target is present at mid
+        if(nums[mid] == target)
+            return mid;
+        // If x target, ignore left half
+        else if(target < nums[mid])
+            high = mid - 1;
+        // If target is smaller, ignore right half
+        else
+            low = mid + 1; 
+    }
+    // If we reach here, then element was not present
+    return -1;
     }
 };
