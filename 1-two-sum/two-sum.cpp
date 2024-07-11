3fc9a8d872415e70febc9fc12d4eed6aa8572209
Sumit Ranjan <sumitranjan.ind@gmail.com>
2024-07-11 12:38:51 +0530

    Time: 10 ms (65.27%) | Memory: 13.6 MB (50.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,7 @@ public:
         map<int, int> mpp;
         for(int i=0; i<n; i++){
             int more = target - nums[i];
-            if(mpp.find(more) != mpp.end() ){
+            if(mpp.find(more) != mpp.end()){
                 return {mpp[more], i};
             }
             mpp[nums[i]] = i;
